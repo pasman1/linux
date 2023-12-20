451920d1ce8d551ea08268ea1c0ef8b295210974
pasman1 <35176855+pasman1@users.noreply.github.com>
2023-12-20 18:08:57 +0100

    Delete grub2 directory

--- grub2/grub-menu-delay.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CONF=/etc/default/grub
-
-sudo sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=4/' $CONF
-sudo sed -i 's/GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=menu/' $CONF
-sudo update-grub
